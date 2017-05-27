getpatchwork/git-pw

commit 2b05496e656ba8a0e9eda4acf814cd6b34b15010

Improve error messages
Make use of the details that the Patchwork instance returns to use,
where possible.

Signed-off-by: Stephen Finucane <stephen@that.guru>

git_pw/api.py
@@ -37,15 +37,25 @@ def get(url, params=None):
     """Make GET request and handle errors."""
     LOG.debug('GET %s', url)
 
-    rsp = requests.get(url, auth=_get_auth(), params=params)
-    if rsp.status_code == 403:
-        LOG.error('Failed to fetch resource: Invalid credentials')
-        LOG.error('Is your git-config correct?')
-        sys.exit(1)
-    elif rsp.status_code != 200:
-        LOG.error('Failed to fetch resource: Invalid URL')
-        LOG.error('Is your git-config correct?')
-        sys.exit(1)
+    try:
+        rsp = requests.get(url, auth=_get_auth(), params=params)
+        rsp.raise_for_status()
+    except requests.exceptions.RequestException as exc:
+        if exc.response is not None and exc.response.content:
+            # we make the assumption that all resposes will be JSON encoded
+            LOG.error(exc.response.json()['detail'])
+            sys
+        else:
+            LOG.error('Failed to fetch resource. Is your configuration '
+                      'correct?')
+            LOG.error("Use the '--debug' flag for more information")
+
+        if CONF.debug:
+            raise
+        else:
+            sys.exit(1)
+
+    LOG.debug('Got response')
 
     return rsp
 
@@ -54,15 +64,23 @@ def put(url, data):
     """Make PUT request and handle errors."""
     LOG.debug('PUT %s, data=%r', url, data)
 
-    rsp = requests.patch(url, auth=_get_auth(), data=data)
-    if rsp.status_code == 403:
-        LOG.error('Failed to update resource: Invalid credentials')
-        LOG.error('Is your git-config correct?')
-        sys.exit(1)
-    elif rsp.status_code != 200:
-        LOG.error('Failed to update resource: Invalid URL')
-        LOG.error('Is your git-config correct?')
-        sys.exit(1)
+    try:
+        rsp = requests.patch(url, auth=_get_auth(), data=data)
+        rsp.raise_for_status()
+    except requests.exceptions.RequestException as exc:
+        if exc.response is not None and exc.response.content:
+            LOG.error(exc.response.json()['detail'])
+        else:
+            LOG.error('Failed to update resource. Is your configuration '
+                      'correct?')
+            LOG.error("Use the '--debug' flag for more information")
+
+        if CONF.debug:
+            raise
+        else:
+            sys.exit(1)
+
+    LOG.debug('Got response')
 
     return rsp.json()
 

git_pw/shell.py
@@ -56,6 +56,7 @@ def cli(debug, username, password, server, project):
     """
     logger.configure_verbosity(debug)
 
+    CONF.debug = debug
     CONF.username = username
     CONF.password = password
     CONF.server = server

commit 53f95c0c7d22d6ef22eb0742e2a144161989d1b9

Remove invalid line
This was a typo. We really need unit tests.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 2b05496 ("Improve error messages")

git_pw/api.py
@@ -44,7 +44,6 @@ def get(url, params=None):
         if exc.response is not None and exc.response.content:
             # we make the assumption that all resposes will be JSON encoded
             LOG.error(exc.response.json()['detail'])
-            sys
         else:
             LOG.error('Failed to fetch resource. Is your configuration '
                       'correct?')