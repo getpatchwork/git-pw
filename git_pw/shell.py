getpatchwork/git-pw

commit 74e117fc1924a2117a578c1b2625b7216e0fe9a8

help: add some command-line help
Using git-pw currently requires digging deep into the source code to
setup the variables.  This is not terribly friendly.

This adds a new subcommand group called 'help'.  The 'help' group is
just meant to provide a quick overview of what's required to get up and
running with git-pw.

Signed-off-by: Aaron Conole <aconole@redhat.com>
Signed-off-by: Stephen Finucane <stephen@that.guru>

git_pw/shell.py
@@ -26,12 +26,33 @@ CONF = config.CONF
               help='Patchwork project.')
 @click.version_option()
 def cli(debug, username, password, server, project):
-    """Interact with a Patchwork instance.
+    """git-pw is a tool for integrating Git with Patchwork.
 
-    Patchwork is a patch tracking system for community-based projects.
-    It is intended to make the patch management process easier for both
-    the project's contributors and maintainers, leaving time for the
-    more important (and more interesting) stuff.
+    git-pw can interact with individual patches, complete patch series, and
+    customized bundles.  The three major subcommands are *patch*, *bundle*,
+    and *series*.
+
+    The git-pw utility is a wrapper which makes REST calls to the Patchwork
+    service. To use git-pw, you must set up your environment by configuring
+    your Patchwork server URL, username, and password.
+
+    Configuring the patchwork URL:
+
+      git config pw.server http://pw.server.com/path/to/patchwork
+
+    Configuring username:
+
+      git config pw.username userid
+
+    Configuring password:
+
+      git config pw.password pass
+
+    Alternatively, you can pass these options via command line parameters or
+    environment variables.
+
+    For more information on any of the commands, simply pass '--help' to the
+    appropriate command.
     """
     logger.configure_verbosity(debug)
 

commit 82d7a0d22a933ddb1f23f8a1f0f945a110b1668b

Use rST formatting in help texts
This ensure things render as expected on ReadTheDocs.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 74e117f ("help: add some command-line help")

git_pw/shell.py
@@ -36,22 +36,22 @@ def cli(debug, username, password, server, project):
     service. To use git-pw, you must set up your environment by configuring
     your Patchwork server URL, username, and password.
 
-    Configuring the patchwork URL:
+    Configuring the patchwork URL::
 
       git config pw.server http://pw.server.com/path/to/patchwork
 
-    Configuring username:
+    Configuring username::
 
       git config pw.username userid
 
-    Configuring password:
+    Configuring password::
 
       git config pw.password pass
 
     Alternatively, you can pass these options via command line parameters or
     environment variables.
 
-    For more information on any of the commands, simply pass '--help' to the
+    For more information on any of the commands, simply pass ``--help`` to the
     appropriate command.
     """
     logger.configure_verbosity(debug)