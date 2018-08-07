a851d3f1bde2b5e9631e8402e7366525ecb5099e
Stephen Finucane <stephen@that.guru>
2018-08-07 16:20:26 +0100

    tests: Don't reuse the same name
    Signed-off-by: Stephen Finucane <stephen@that.guru>
    Closes: a2ab9b3 ("Add missing argument for _echo_via_pager call")

--- git_pw/tests/test_utils.py
+++ git_pw/tests/test_utils.py
@@ -56,7 +56,7 @@ def test_echo_via_pager_env_PAGER(mock_inner, mock_config):
 
 @mock.patch.object(utils, 'git_config', return_value=None)
 @mock.patch.object(utils, '_echo_via_pager')
-def test_echo_via_pager_env_PAGER(mock_inner, mock_config):
+def test_echo_via_pager_env_default(mock_inner, mock_config):
     utils.echo_via_pager('test')
 
     mock_config.assert_called_once_with('core.parser')
