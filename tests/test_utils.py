7a12b8117658b4459a7a08220c8bc4551eb5964a
Stephen Finucane <stephen@that.guru>
2018-11-07 23:57:09 +0000

    Add support for multiple output formats
    Three are provided:
    
    - table (default, same as what we currently have)
    - simple (less markup than default but still legible)
    - csv (comma-separated output)
    
    This can be configured via git-config, an environment option, or a
    command line argument.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,39 +34,47 @@ def test_git_config_error(mock_subprocess):
 
 
 @mock.patch.object(utils, 'git_config', return_value='bar')
+@mock.patch.object(utils, '_tabulate')
 @mock.patch.object(utils, '_echo_via_pager')
 @mock.patch.dict(os.environ, {'GIT_PAGER': 'foo', 'PAGER': 'baz'})
-def test_echo_via_pager_env_GIT_PAGER(mock_inner, mock_config):
-    utils.echo_via_pager('test')
+def test_echo_via_pager_env_GIT_PAGER(mock_inner, mock_tabulate, mock_config):
+    utils.echo_via_pager('test', ('foo',), None)
 
     mock_config.assert_not_called()
-    mock_inner.assert_called_once_with('foo', 'test')
+    mock_tabulate.assert_called_once_with('test', ('foo',), None)
+    mock_inner.assert_called_once_with('foo', mock_tabulate.return_value)
 
 
 @mock.patch.object(utils, 'git_config', return_value='bar')
+@mock.patch.object(utils, '_tabulate')
 @mock.patch.object(utils, '_echo_via_pager')
 @mock.patch.dict(os.environ, {'PAGER': 'baz'})
-def test_echo_via_pager_config(mock_inner, mock_config):
-    utils.echo_via_pager('test')
+def test_echo_via_pager_config(mock_inner, mock_tabulate, mock_config):
+    utils.echo_via_pager('test', ('foo',), None)
 
     mock_config.assert_called_once_with('core.parser')
-    mock_inner.assert_called_once_with('bar', 'test')
+    mock_tabulate.assert_called_once_with('test', ('foo',), None)
+    mock_inner.assert_called_once_with('bar', mock_tabulate.return_value)
 
 
 @mock.patch.object(utils, 'git_config', return_value=None)
+@mock.patch.object(utils, '_tabulate')
 @mock.patch.object(utils, '_echo_via_pager')
 @mock.patch.dict(os.environ, {'PAGER': 'baz'})
-def test_echo_via_pager_env_PAGER(mock_inner, mock_config):
-    utils.echo_via_pager('test')
+def test_echo_via_pager_env_PAGER(mock_inner, mock_tabulate, mock_config):
+    utils.echo_via_pager('test', ('foo',), None)
 
     mock_config.assert_called_once_with('core.parser')
-    mock_inner.assert_called_once_with('baz', 'test')
+    mock_tabulate.assert_called_once_with('test', ('foo',), None)
+    mock_inner.assert_called_once_with('baz', mock_tabulate.return_value)
 
 
 @mock.patch.object(utils, 'git_config', return_value=None)
+@mock.patch.object(utils, '_tabulate')
 @mock.patch.object(utils, '_echo_via_pager')
-def test_echo_via_pager_env_default(mock_inner, mock_config):
-    utils.echo_via_pager('test')
+def test_echo_via_pager_env_default(mock_inner, mock_tabulate, mock_config):
+    utils.echo_via_pager('test', ('foo',), None)
 
     mock_config.assert_called_once_with('core.parser')
-    mock_inner.assert_called_once_with('less', 'test')
+    mock_tabulate.assert_called_once_with('test', ('foo',), None)
+    mock_inner.assert_called_once_with('less', mock_tabulate.return_value)
