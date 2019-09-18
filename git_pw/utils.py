getpatchwork/git-pw

commit 63e8ea1f0289fd6a7cc85c588d4d0410fde5ee2d

Resolves issues with CSV printing on Python 2
Add lots of unit tests to ensure we don't regress.

Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: f3974aa ("utils: Wrap CSV in quotes")

git_pw/utils.py
@@ -11,18 +11,8 @@ import subprocess
 import sys
 
 import click
+import six
 from tabulate import tabulate
-
-
-if sys.version_info < (3, 0):
-    from io import BytesIO  # noqa
-else:
-    from io import StringIO  # noqa
-
-if sys.version_info < (3, 0):
-    _text = unicode  # noqa
-else:
-    _text = str  # noqa
 
 
 def trim(string, length=70):  # type: (str, int) -> str
@@ -70,11 +60,13 @@ def _tabulate(output, headers, fmt):
     elif fmt == 'simple':
         return tabulate(output, headers, tablefmt='simple')
     elif fmt == 'csv':
-        result = StringIO()
-        writer = csv.writer(result, quoting=csv.QUOTE_ALL)
-        writer.writerow(headers)
+        result = six.StringIO()
+        writer = csv.writer(
+            result, quoting=csv.QUOTE_ALL, lineterminator=os.linesep)
+        writer.writerow([six.ensure_str(h) for h in headers])
         for item in output:
-            writer.writerow(_text(x if x is not None else '') for x in item)
+            writer.writerow([
+                six.ensure_str(x if x is not None else '') for x in item])
         return result.getvalue()
 
     print('pw.format must be one of: table, simple, csv')

tests/test_utils.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
+
 """Unit tests for ``git_pw/utils.py``."""
 
 import subprocess
+import textwrap
 import os
 
 import mock
@@ -78,3 +81,59 @@ def test_echo_via_pager_env_default(mock_inner, mock_tabulate, mock_config):
     mock_config.assert_called_once_with('core.parser')
     mock_tabulate.assert_called_once_with('test', ('foo',), None)
     mock_inner.assert_called_once_with('less', mock_tabulate.return_value)
+
+
+def _test_tabulate(fmt):
+    output = [(b'foo', 'bar', u'baz', '😀', None)]
+    headers = ('col1', 'colb', 'colIII', 'colX', 'colY')
+
+    result = utils._tabulate(output, headers, fmt)
+
+    return output, headers, result
+
+
+@mock.patch.object(utils, 'tabulate')
+def test_tabulate_table(mock_tabulate):
+    output, headers, result = _test_tabulate('table')
+
+    mock_tabulate.assert_called_once_with(output, headers, tablefmt='psql')
+    assert result == mock_tabulate.return_value
+
+
+@mock.patch.object(utils, 'tabulate')
+def test_tabulate_simple(mock_tabulate):
+    output, headers, result = _test_tabulate('simple')
+
+    mock_tabulate.assert_called_once_with(output, headers, tablefmt='simple')
+    assert result == mock_tabulate.return_value
+
+
+@mock.patch.object(utils, 'tabulate')
+def test_tabulate_csv(mock_tabulate):
+    output, headers, result = _test_tabulate('csv')
+
+    mock_tabulate.assert_not_called()
+    assert result == textwrap.dedent("""\
+        "col1","colb","colIII","colX","colY"
+        "foo","bar","baz","😀",""
+    """)
+
+
+@mock.patch.object(utils, 'git_config', return_value='simple')
+@mock.patch.object(utils, 'tabulate')
+def test_tabulate_git_config(mock_tabulate, mock_git_config):
+    output, headers, result = _test_tabulate(None)
+
+    mock_git_config.assert_called_once_with('pw.format')
+    mock_tabulate.assert_called_once_with(output, headers, tablefmt='simple')
+    assert result == mock_tabulate.return_value
+
+
+@mock.patch.object(utils, 'git_config', return_value='')
+@mock.patch.object(utils, 'tabulate')
+def test_tabulate_default(mock_tabulate, mock_git_config):
+    output, headers, result = _test_tabulate(None)
+
+    mock_git_config.assert_called_once_with('pw.format')
+    mock_tabulate.assert_called_once_with(output, headers, tablefmt='psql')
+    assert result == mock_tabulate.return_value

commit 1a71697687e12cafbbffc8e4fb3c86a3841eaf64

Handle integers too when printing CSV
Signed-off-by: Stephen Finucane <stephen@that.guru>
Fixes: 63e8ea1 ("Resolves issues with CSV printing on Python 2")

git_pw/utils.py
@@ -15,6 +15,15 @@ import six
 from tabulate import tabulate
 
 
+def ensure_str(s):
+    if s is None:
+        s = ''
+    elif not isinstance(s, (six.text_type, six.binary_type)):
+        s = str(s)
+
+    return six.ensure_str(s)
+
+
 def trim(string, length=70):  # type: (str, int) -> str
     """Trim a string to the given length."""
     return (string[:length - 1] + '...') if len(string) > length else string
@@ -63,10 +72,9 @@ def _tabulate(output, headers, fmt):
         result = six.StringIO()
         writer = csv.writer(
             result, quoting=csv.QUOTE_ALL, lineterminator=os.linesep)
-        writer.writerow([six.ensure_str(h) for h in headers])
+        writer.writerow([ensure_str(h) for h in headers])
         for item in output:
-            writer.writerow([
-                six.ensure_str(x if x is not None else '') for x in item])
+            writer.writerow([ensure_str(i) for i in item])
         return result.getvalue()
 
     print('pw.format must be one of: table, simple, csv')

tests/test_utils.py
@@ -84,8 +84,8 @@ def test_echo_via_pager_env_default(mock_inner, mock_tabulate, mock_config):
 
 
 def _test_tabulate(fmt):
-    output = [(b'foo', 'bar', u'baz', '😀', None)]
-    headers = ('col1', 'colb', 'colIII', 'colX', 'colY')
+    output = [(b'foo', 'bar', u'baz', '😀', None, 1)]
+    headers = ('col1', 'colb', 'colIII', 'colX', 'colY', 'colZ')
 
     result = utils._tabulate(output, headers, fmt)
 
@@ -114,8 +114,8 @@ def test_tabulate_csv(mock_tabulate):
 
     mock_tabulate.assert_not_called()
     assert result == textwrap.dedent("""\
-        "col1","colb","colIII","colX","colY"
-        "foo","bar","baz","😀",""
+        "col1","colb","colIII","colX","colY","colZ"
+        "foo","bar","baz","😀","","1"
     """)
 
 